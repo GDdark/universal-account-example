72316767e02471b76df10e0465e0311c5e55e6a8
JaxGG <JaxGG@particle.network>
2025-05-06 16:10:27 +0800

    feat: add convert example

---
+++ examples/convert-evm.ts
@@ -0,0 +1,32 @@
+import { config } from 'dotenv';
+import { CHAIN_ID, SUPPORTED_TOKEN_TYPE, UniversalAccount } from '@GDdark/universal-account';
+import { getBytes, Wallet } from 'ethers';
+
+config();
+
+(async () => {
+    const wallet = new Wallet(process.env.PRIVATE_KEY || '');
+    const universalAccount = new UniversalAccount({
+        projectId: process.env.PROJECT_ID || '',
+        ownerAddress: wallet.address,
+        tradeConfig: {
+            universalGas: true,
+        },
+    });
+
+    const smartAccountOptions = await universalAccount.getSmartAccountOptions();
+    console.log('Your UA EVM Address:', smartAccountOptions.smartAccountAddress);
+    console.log('Your UA Solana Address:', smartAccountOptions.solanaSmartAccountAddress);
+
+    const transaction = await universalAccount.createConvertTransaction({
+        expectToken: { type: SUPPORTED_TOKEN_TYPE.USDC, amount: '0.0001' },
+        chainId: CHAIN_ID.ARBITRUM_MAINNET_ONE,
+    });
+
+    console.log('convert transaction', transaction);
+
+    const sendResult = await universalAccount.sendTransaction(transaction, wallet.signMessageSync(getBytes(transaction.rootHash)));
+
+    console.log('sendResult', sendResult);
+    console.log('explorer url', `https://universalx.app/activity/details?id=${sendResult.transactionId}`);
+})();
