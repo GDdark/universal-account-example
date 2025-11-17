bb812ab3a3b6d4175d6ba443b356aa41e7769d57
JaxGG <JaxGG@particle.network>
2025-11-17 14:31:42 +0800

    add example

--- examples/warmup.ts
+++ examples/warmup.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 import { CHAIN_ID, UniversalAccount } from '@particle-network/universal-account-sdk';
-import { getBytes, Wallet } from 'ethers';
+import { Wallet } from 'ethers';
 
 config();
 
@@ -8,8 +8,8 @@ config();
     const wallet = new Wallet(process.env.PRIVATE_KEY || '');
     const universalAccount = new UniversalAccount({
         projectId: process.env.PROJECT_ID || '',
-        projectClientKey: process.env.PROJECT_CLIENT_KEY || "",
-        projectAppUuid: process.env.PROJECT_APP_UUID || "",
+        projectClientKey: process.env.PROJECT_CLIENT_KEY || '',
+        projectAppUuid: process.env.PROJECT_APP_UUID || '',
         ownerAddress: wallet.address,
         tradeConfig: {
             universalGas: true,
@@ -20,12 +20,24 @@ config();
     console.log('Your UA EVM Address:', smartAccountOptions.smartAccountAddress);
     console.log('Your UA Solana Address:', smartAccountOptions.solanaSmartAccountAddress);
 
-    await universalAccount.warmUpToken({
+    const result = await universalAccount.getTokenPair({
         chainId: CHAIN_ID.ARBITRUM_MAINNET_ONE,
         address: '0x912CE59144191C1204E64559FE8253a0e49E6548',
     });
 
-    console.log('warmup token success');
+    console.log('get token pair success');
 
-    
+    const transaction = await universalAccount.createBuyTransaction(
+        {
+            token: { chainId: CHAIN_ID.ARBITRUM_MAINNET_ONE, address: '0x912CE59144191C1204E64559FE8253a0e49E6548' },
+            amountInUSD: '0.001',
+        },
+        {
+            tokenPair: {
+                address: result.pair.address,
+                factory: result.pair.factory,
+            },
+        },
+    );
+    console.log('buy transaction', transaction);
 })();
